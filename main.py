14e1098ca91e0b9451fc08d60a8b7c4dd738396e
nw4167 <nattygwa@gmail.com>
2025-09-19 14:33:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("Welcome to the clothing shop Chatbot")
+print("Welcome to the Clothing Shop Chatbot")
 name = input("What is your name? ")
 age = input("Hello " + name + ", how old are you? ")
+
+print("how can I help you today?")
+print("1. Browse clothes")
+print("2. Check store hours")
+print("3. Talk to customer service")
+print("4. Exit the store")
