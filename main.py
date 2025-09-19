4bc0bf643bdb61731ddb1472ea5a8e8fee0f8e37
nw4167 <nattygwa@gmail.com>
2025-09-19 14:54:04 -0500

    Updated- 9/19/25 main.py

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ print("1. Browse clothes")
 print("2. Check store hours")
 print("3. Talk to customer service")
 print("4. Exit the store")
+
+choice = input("Choose an option (1-4): ")
+
+if choice == "1":
+    print("*you look through clothes and you buy the one you like*")
+elif choice == "2":
+    print("*you see the store hours and find out it hasn't opened yet...😔*")
+elif choice == "3":
+    print("*you speak with the manager*")
+elif choice == "4":
+    print("Have a good day and thanks for visiting, " + name + "!")
+else:
+    print("dude, thats not in the option list...")
