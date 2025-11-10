43e2c8b8320e5bf7730b55c90ad421a31f36a3f2
nw6417 <nattygwa@gmail.com>
2025-11-10 23:29:10 +0000

    11/10/25

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Welcome to the Clothing Shop Chatbot")
+print("Welcome to the Clothing Shop Website!")
 name = input("What is your name? ")
 age = input("Hello " + name + ", how old are you? ")
 if int(age) < 18:
@@ -9,19 +9,19 @@ else:
 
 print("How can I help you today?")
 print("1. Browse clothes")
-print("2. Check store hours")
+print("2. Check in-person store hours")
 print("3. Talk to customer service")
 print("4. Exit the store")
 
 choice = input("Choose an option (1-4): ")
 
 if choice == "1":
-    print("*You look through clothes and you buy the one you like*")
+    print("(redirect to clothing page*)")
 elif choice == "2":
     print("*Store Hours: Mon–Fri: Open, Sat: Open, Sun: Closed*")
 elif choice == "3":
-    print("*You speak with the manager*")
+    print("*redirect to human customer-service employee*")
 elif choice == "4":
     print("Have a good day and thanks for visiting, " + name + "!")
 else:
-    print("thats not in the option list...")
+    print("Please choose from the provided options...")
